ecac74428ab82fd13159381e4e1c98caa519a25a
VinutaYalagi <yalagivinuta@gmail.com>
2026-08-28 11:31:08 +0530

    modified the changes

--- names.py
+++ names.py
@@ -1,3 +1,6 @@
 name=input("Enter your name")
 surname = input("Enter your surname")
+contact = input("enter your contact details")
+
 print("Your details:",name+" "+surname)
+print("Your contact is:",contact)
